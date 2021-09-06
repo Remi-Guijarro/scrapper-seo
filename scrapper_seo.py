a4a4f3dde3ee2069fe145216de5965040659239f
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-09-06 14:29:37 +0200

    Added arguments to customize research based on keywords

--- scrapper_seo.py
+++ scrapper_seo.py
@@ -2,21 +2,37 @@ from time import sleep
 from typing import BinaryIO
 from selenium import webdriver
 from selenium import *
+from sys import argv
 
-option = webdriver.ChromeOptions()
-option.add_argument('headless')
-chromeBinary = "C:/Users/remig/Desktop/chromedriver_win32/chromedriver.exe"
-driver = webdriver.Chrome(chromeBinary, options=option)
-driver.get("https://www.google.com/search?q=allintitle%3A\"voiture+électrique\"")
 
-cookies_btn = driver.find_elements_by_class_name('jyfHyd')
+def search_keywords_result_count(keywords):
+    request = "https://www.google.com/search?q=allintitle%3A"
+    for index,key in enumerate(keywords):
+        if index == 0:
+            request += key
+        else:
+            request += "+" + key
+    print(request)
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    chromeBinary = "C:/Users/remig/Desktop/chromedriver_win32/chromedriver.exe"
+    driver = webdriver.Chrome(chromeBinary, options=option)
+    driver.get(request)
 
-for btn in cookies_btn:
-    if btn.text == 'J\'accepte':
-        btn.click()
+    cookies_btn = driver.find_elements_by_class_name('jyfHyd')
 
-result_stat = driver.find_element_by_id('result-stats')
-print(str(result_stat.text))
+    for btn in cookies_btn:
+        if btn.text == 'J\'accepte':
+            btn.click()
+
+    result_stat = driver.find_element_by_id('result-stats')
+    print(str(result_stat.text))
+
+
+if __name__ == "__main__":
+    print(argv[1:])
+    search_keywords_result_count(argv[1:])
+
 
 
 # next_btn = driver.find_element_by_id('pnnext')
